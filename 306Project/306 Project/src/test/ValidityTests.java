5bfde79e92e88c531f8e3415f3324ec8eb2063d9
JSCooke <jaydencooke@outlook.com>
2016-07-29 15:47:50 +1200

    Renamed Class and created template methods.
    Empty JUnit test methods created.

---
+++ 306Project/306 Project/src/test/ValidityTests.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JSCooke on 29/07/16.
+ */
+public class ValidityTests {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void validTest(){
+        //Create a graph with a known optimal path, and verify that the optimal path has been found.
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+}
