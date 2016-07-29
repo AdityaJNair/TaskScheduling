409a64b926f70ea9c8a35d6aa0fdfdb886babe99
JSCooke <jaydencooke@outlook.com>
2016-07-29 15:12:47 +1200

    Created Test folder and rudimentary JUnit test

---
+++ 306Project/306 Project/src/test/ValidityTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JSCooke on 29/07/16.
+ */
+public class ValidityTest {
+
+    @Test
+    public void validTest(){
+        assertTrue(true);
+    }
+}
