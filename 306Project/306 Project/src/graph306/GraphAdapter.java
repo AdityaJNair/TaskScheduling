8d0bf054fc8a28f2ade44d466e5b1c898aca55bc
JSCooke <jaydencooke@outlook.com>
2016-08-04 12:56:41 +1200

    Implemented the GraphAdapter class

---
+++ 306Project/306 Project/src/graph306/GraphAdapter.java
@@ -0,0 +1,55 @@
+package graph306;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
+/**
+ * A wrapper class so that the graph representation framework in use can be altered without changing methods.
+ * Follows the adapter design pattern.
+ */
+
+public class GraphAdapter {
+
+    public Graph graph = new SingleGraph("Graph under construction");
+
+    /**
+     * Returns the graph that has been created.
+     * @return The graph, as an org.graphstream.graph.Graph.
+     */
+
+    public Graph getGraph() {
+
+        return graph;
+
+    }
+
+    /**
+     * Adds a node to the graph.
+     * @param id The unique identifier for the node.
+     * @param cost The cost (in time units) of the node.
+     */
+
+    public void addNode(String id, int cost){
+
+        graph.addNode(id);
+        graph.getNode(id).addAttribute("Cost", cost);
+
+    }
+
+    /**
+     * Adds a directed edge to the graph.
+     * @param id The unique identifier for the edge.
+     * @param source The identifier of the parent node of the edge.
+     * @param destination The identifier of the child node of the edge.
+     * @param cost The cost (in time units) of the node.
+     */
+
+    public void addEdge(String id, String source, String destination, int cost){
+
+        graph.addEdge(id, source, destination);
+        graph.getEdge(id).addAttribute("Cost", cost);
+        graph.getEdge(id).isDirected();
+
+    }
+
+}
