74cf91ff60ef552e8f1dff6aa30f23e1bc2bd2db
JSCooke <jaydencooke@outlook.com>
2016-08-04 13:53:08 +1200

    Reformatted UserOptions class

--- 306Project/306 Project/src/graph306/UserOptions.java
+++ 306Project/306 Project/src/graph306/UserOptions.java
@@ -4,9 +4,10 @@ package graph306;
  * Class to hold the user's input command line parameters.
  */
 
-//This may become a singleton class if we need to use it later.
+
 public class UserOptions {
 
+    //Data fields
     private String filenameIn;
     private String filenameOut;
     private boolean isVisible;
@@ -14,50 +15,40 @@ public class UserOptions {
     private int parallelThreads;
     private int processors;
 
+    //Getters and setters for all fields.
     public String getFilenameIn() {
         return filenameIn;
     }
-
     public void setFilenameIn(String filenameIn) {
         this.filenameIn = filenameIn;
     }
-
     public String getFilenameOut() {
         return filenameOut;
     }
-
     public void setFilenameOut(String filenameOut) {
         this.filenameOut = filenameOut;
     }
-
     public boolean isVisible() {
         return isVisible;
     }
-
     public void setVisible(boolean visible) {
         isVisible = visible;
     }
-
     public boolean isParallel() {
         return isParallel;
     }
-
     public void setParallel(boolean parallel) {
         isParallel = parallel;
     }
-
     public int getParallelThreads() {
         return parallelThreads;
     }
-
     public void setParallelThreads(int parallelThreads) {
         this.parallelThreads = parallelThreads;
     }
-
     public int getProcessors() {
         return processors;
     }
-
     public void setProcessors(int processors) {
         this.processors = processors;
     }
