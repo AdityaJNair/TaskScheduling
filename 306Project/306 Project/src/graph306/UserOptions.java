951cf6badec23f52cb3533d8637677fb6e5fa4f6
JSCooke <jaydencooke@outlook.com>
2016-08-04 13:13:03 +1200

    Added fields to UserOptions class

--- 306Project/306 Project/src/graph306/UserOptions.java
+++ 306Project/306 Project/src/graph306/UserOptions.java
@@ -4,4 +4,11 @@ package graph306;
  * Class to hold the user's input command line parameters.
  */
 public class UserOptions {
+    private String filenameIn;
+    private String filenameOut;
+    private boolean isVisible;
+    private boolean isParallel;
+    private int parallelThreads;
+    private int processors;
+
 }
