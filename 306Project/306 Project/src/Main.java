d271636ff33c4344ccb5e3e25ecafa70281652aa
JSCooke <jaydencooke@outlook.com>
2016-08-04 15:57:54 +1200

    Removed unneeded imports from main

--- 306Project/306 Project/src/Main.java
+++ 306Project/306 Project/src/Main.java
@@ -1,19 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.graphstream.graph.Graph;
-import org.graphstream.graph.implementations.DefaultGraph;
-import org.graphstream.graph.implementations.SingleGraph;
-import org.graphstream.stream.file.FileSource;
-import org.graphstream.stream.file.FileSourceDOT;
-import org.graphstream.stream.file.FileSourceFactory;
 
 import graph306.CustomGraphReader;
-import graph306.UserOptions;
 
 public class Main {
 
