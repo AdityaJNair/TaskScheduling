87fb9dd4e07b8abb0a679f2c77342cecc1b0a26b
JSCooke <jaydencooke@outlook.com>
2016-08-04 13:32:55 +1200

    Created CustomGraphReader class

---
+++ 306Project/306 Project/src/graph306/CustomGraphReader.java
@@ -0,0 +1,8 @@
+package graph306;
+
+/**
+ * Class to read in a directed graph from a .dot file and create a directed graph.
+ * Also notes which nodes have no dependents.
+ */
+public class CustomGraphReader {
+}
