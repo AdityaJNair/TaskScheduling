4a0aa07336eaca51f1f5d9c7bc6e7912e551e6f2
PriyankitSingh <psin778@aucklanduni.ac.nz>
2016-08-04 13:41:47 +1200

    Added skeleton code to CustomGraphReader

--- 306Project/306 Project/src/graph306/CustomGraphReader.java
+++ 306Project/306 Project/src/graph306/CustomGraphReader.java
@@ -1,8 +1,26 @@
 package graph306;
 
+import java.awt.List;
+import java.util.HashSet;
+
 /**
  * Class to read in a directed graph from a .dot file and create a directed graph.
  * Also notes which nodes have no dependents.
  */
 public class CustomGraphReader {
+	UserOptions userOptions;
+	HashSet sourceNodes;
+	List edgeList;
+	
+	public CustomGraphReader(String[] args){
+		
+	}
+	
+	private void readDAG(){
+		
+	}
+	
+	private void createDAG(){
+		
+	}
 }
