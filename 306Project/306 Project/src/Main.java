49b585ce3d316540cc25b7620952b6c7d61381df
PriyankitSingh <psin778@aucklanduni.ac.nz>
2016-08-04 15:15:29 +1200

    Cleaned up main.java

--- 306Project/306 Project/src/Main.java
+++ 306Project/306 Project/src/Main.java
@@ -12,6 +12,8 @@ import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceDOT;
 import org.graphstream.stream.file.FileSourceFactory;
 
+import graph306.CustomGraphReader;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -20,46 +22,8 @@ public class Main {
 		System.out.print("Enter file path: "); // Use args[0] in the real program		
 		String filePath = reader.nextLine();
 		
-		Graph graph = null;
+		CustomGraphReader graphReader = new CustomGraphReader(args);
 		
-		try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-		    for(String line; (line = br.readLine()) != null; ) {
-		        if(line.contains("digraph")){
-		        	Pattern p = Pattern.compile("\"([^\"]*)\"");
-		        	Matcher m = p.matcher(line);
-		        	while (m.find()) { // first line. Contains graph name
-		        	  System.out.println("Name of graph : " + m.group(1));
-		        	  graph = new SingleGraph(m.group(1)); // create new graph
-		        	}
-		        	continue;
-		        } else if(line.contains("->")){ // Line with edges and weights
-		        	String[] words = line.split("\\s+");
-		        	System.out.println(words[0] + " to " + words[2]);
-		        	graph.addEdge(words[0]+words[2], words[0], words[2], true);
-		        } else { // Vertices
-		        	if(line.contains("}")) continue;
-		        	String[] words = line.split("\\s+");
-		        	graph.addNode(words[0]);
-		        	System.out.println(words[0]);
-		        }
-		    }
-		}
-		
-		graph.display();
-		
-	}
-	
-	public void createSampleGraph(){
-		Graph graph = new SingleGraph("Sample");
-		
-		graph.addNode("A" );
-		graph.addNode("B" );
-		graph.addNode("C" );
-		graph.addEdge("AB", "A", "B");
-		graph.addEdge("BC", "B", "C");
-		graph.addEdge("CA", "C", "A");
-		
-		graph.display();
 	}
 
 }
