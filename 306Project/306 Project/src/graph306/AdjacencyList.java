e1ae6972a90544ebfbb4c8545340508e8572947e
JSCooke <jaydencooke@outlook.com>
2016-08-06 17:05:10 +1200

    Created fields in AdjacencyList class

--- 306Project/306 Project/src/graph306/AdjacencyList.java
+++ 306Project/306 Project/src/graph306/AdjacencyList.java
@@ -1,10 +1,19 @@
 package graph306;
 
+import java.util.*;
+
 /**
  * Created by JSCooke on 6/08/16.
  */
 public class AdjacencyList {
 
-    
+    //List of lists containing nodes and the edge weight to them.
+    private List<List<Map<String,Integer>>> adjacencyList = new ArrayList<>();
+
+    //Maps nodes to their index in the adjacency list.
+    private Map<String, Integer> indexes = new HashMap<>();
+
+    //Maps nodes to their weights.
+    private Map<String, Integer> weights = new HashMap<>();
 
 }
