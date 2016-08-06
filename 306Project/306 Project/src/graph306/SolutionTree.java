6914b739d1058023a347b880699fe4d769f01c67
PriyankitSingh <psin778@aucklanduni.ac.nz>
2016-08-06 17:41:51 +1200

    Added fields to SolutionTree class

--- 306Project/306 Project/src/graph306/SolutionTree.java
+++ 306Project/306 Project/src/graph306/SolutionTree.java
@@ -1,5 +1,26 @@
 package graph306;
 
-public class SolutionTree {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * This class creates a solution tree from the input adjacency list.
+ * @author Priyankit
+ *
+ */
+public class SolutionTree {
+	
+	// Stores the time for the current shortest schedule.
+	private static int minimumTime = Integer.MAX_VALUE;
+	
+	// A list containing the current best schedule.
+	private static List<NodeObject> bestSchedule = new ArrayList<NodeObject>();
+	
+	public AdjacencyList inputGraph;
+	
+	public SolutionTree(AdjacencyList inputGraph){
+		this.inputGraph = inputGraph;
+	}
+	
+	
 }
