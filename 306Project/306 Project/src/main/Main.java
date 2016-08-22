4507c1830d00d122f1a34409c00c333279565bdd
JunoJin <jjin525@aucklanduni.ac.nz>
2016-08-22 15:32:28 +1200

    Update Main.java

--- 306Project/306 Project/src/main/Main.java
+++ 306Project/306 Project/src/main/Main.java
@@ -3,9 +3,8 @@ package main;
 import graph306.CustomGraphReader;
 import graph306.SolutionTree;
 import graph306.UserOptions;
-import org.graphstream.ui.view.View;
+import org.graphstream.ui.swingViewer.ViewPanel;
 import org.graphstream.ui.view.Viewer;
-import org.graphstream.ui.view.Viewer.ThreadingModel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,46 +12,68 @@ import java.io.IOException;
 
 public class Main {
 
-    public static View view;
+    public static ViewPanel view;
 
-    public static class MyFrame extends JFrame
-    {
-        //private Graph graph = new MultiGraph("embedded");
-        //private Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-
-        private Viewer viewer = new Viewer(SolutionTree.gsGraph, ThreadingModel.GRAPH_IN_GUI_THREAD);
-        private View view = viewer.addDefaultView(false);
-
-        public MyFrame() {
-            setLayout(new BorderLayout());
-            this.add((Component) view);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-        }
-    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
 		//start timing
 		//long startTime = System.nanoTime();
 
         System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-        SolutionTree.gsGraph.addAttribute("ui.stylesheet", "node { " +
-                "size:10px; " +
+        SolutionTree.bestTimeTree.addAttribute("ui.stylesheet", "node { " +
+                "size:6px; " +
                 "text-color:red; " +
-                "text-size:20px; " +
+                "text-size:9px; " +
                 "text-alignment:above;" +
                 "text-background-mode:rounded-box;" +
                 "}" +
                 "edge { " +
                 "text-color:red; " +
-                "text-size:10px; " +
+                "text-size:13px; " +
                 "text-background-mode:rounded-box;" +
                 "size:3px;" +
+                "}" +
+                "graph {" +
+                "fill-color:yellow;" +
                 "}");
-        SolutionTree.gsGraph.addAttribute("ui.antialias");
-        SolutionTree.gsGraph.setStrict(false);
-        SolutionTree.gsGraph.addAttribute("ui.quality");
-        Viewer viewer = SolutionTree.gsGraph.display();
-        view = viewer.getDefaultView();
+        SolutionTree.datGraph.addAttribute("ui.stylesheet", "node { " +
+                "size:30px; " +
+                "text-color:red; " +
+                "text-size:15px; " +
+                "stroke-mode:plain;" +
+                "fill-color:magenta;" +
+                "}" +
+                "edge { " +
+                "text-color:red; " +
+                "text-size:13px; " +
+                "text-background-mode:rounded-box;" +
+                "size:1px;" +
+                "}");
+        SolutionTree.bestTimeTree.addAttribute("ui.antialias");
+        SolutionTree.bestTimeTree.setStrict(false);
+        SolutionTree.bestTimeTree.addAttribute("ui.quality");
+        SolutionTree.datGraph.addAttribute("ui.antialias");
+        SolutionTree.datGraph.setStrict(false);
+        SolutionTree.datGraph.addAttribute("ui.quality");
+        Viewer treeViewer = new Viewer(SolutionTree.bestTimeTree, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+        Viewer graphViewer = new Viewer(SolutionTree.datGraph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+        graphViewer.enableAutoLayout();
+        JFrame frame = new JFrame("Visualization");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        Dimension dim = new Dimension(500,300);
+        ViewPanel tV = treeViewer.addDefaultView(false);
+
+        tV.setPreferredSize(dim);
+        panel.add(tV, BorderLayout.LINE_END);
+        ViewPanel gV = graphViewer.addDefaultView(false);
+        gV.setPreferredSize(dim);
+        panel.add(gV, BorderLayout.LINE_START);
+//        panel.add(gV, BorderLayout.PAGE_END);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+        frame.pack();
 //        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 //        view.resizeFrame(screenSize.getWidth(), screenSize.getHeight());
 //        view.getCamera().setViewCenter(440000, 2503000, 0);
@@ -81,6 +102,8 @@ public class Main {
 		//Create .dot file at the end
 		DotWriter writer = new DotWriter();
 		writer.createDot(solver.getBestSchedule(),UserOptions.getInstance(),graphReader.getEdgeList(),solver.getInputGraph());
+
+        SolutionTree.bestTimeTree.addAttribute("ui.stylesheet", "graph{fill-color: rgb(60,141,13);}");
 	}
 
 }
