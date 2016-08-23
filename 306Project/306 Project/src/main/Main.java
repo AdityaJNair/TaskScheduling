f595e3b0f2b9a72585a1c7b5876046311d87265a
Juno <jjin525@aucklanduni.ac.nz>
2016-08-23 18:18:50 +1200

    Updated/finished stats for visualization

--- 306Project/306 Project/src/main/Main.java
+++ 306Project/306 Project/src/main/Main.java
@@ -1,109 +1,152 @@
 package main;
-
+import data_structures.NodeObject;
+import data_structures.UserOptions;
 import graph306.CustomGraphReader;
 import graph306.SolutionTree;
-import graph306.UserOptions;
 import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.View;
 import org.graphstream.ui.view.Viewer;
+import visual.SolutionTreeVisual;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
 public class Main {
-
-    public static ViewPanel view;
-
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-		//start timing
-		//long startTime = System.nanoTime();
-
-        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-        SolutionTree.bestTimeTree.addAttribute("ui.stylesheet", "node { " +
-                "size:6px; " +
-                "text-color:red; " +
-                "text-size:9px; " +
-                "text-alignment:above;" +
-                "text-background-mode:rounded-box;" +
-                "}" +
-                "edge { " +
-                "text-color:red; " +
-                "text-size:13px; " +
-                "text-background-mode:rounded-box;" +
-                "size:3px;" +
-                "}" +
-                "graph {" +
-                "fill-color:yellow;" +
-                "}");
-        SolutionTree.datGraph.addAttribute("ui.stylesheet", "node { " +
-                "size:30px; " +
-                "text-color:red; " +
-                "text-size:15px; " +
-                "stroke-mode:plain;" +
-                "fill-color:magenta;" +
-                "}" +
-                "edge { " +
-                "text-color:red; " +
-                "text-size:13px; " +
-                "text-background-mode:rounded-box;" +
-                "size:1px;" +
-                "}");
-        SolutionTree.bestTimeTree.addAttribute("ui.antialias");
-        SolutionTree.bestTimeTree.setStrict(false);
-        SolutionTree.bestTimeTree.addAttribute("ui.quality");
-        SolutionTree.datGraph.addAttribute("ui.antialias");
-        SolutionTree.datGraph.setStrict(false);
-        SolutionTree.datGraph.addAttribute("ui.quality");
-        Viewer treeViewer = new Viewer(SolutionTree.bestTimeTree, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-        Viewer graphViewer = new Viewer(SolutionTree.datGraph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-        graphViewer.enableAutoLayout();
-        JFrame frame = new JFrame("Visualization");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel(new BorderLayout());
-        Dimension dim = new Dimension(500,300);
-        ViewPanel tV = treeViewer.addDefaultView(false);
-
-        tV.setPreferredSize(dim);
-        panel.add(tV, BorderLayout.LINE_END);
-        ViewPanel gV = graphViewer.addDefaultView(false);
-        gV.setPreferredSize(dim);
-        panel.add(gV, BorderLayout.LINE_START);
-//        panel.add(gV, BorderLayout.PAGE_END);
-        frame.getContentPane().add(panel);
-        frame.setVisible(true);
-        frame.pack();
-//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-//        view.resizeFrame(screenSize.getWidth(), screenSize.getHeight());
-//        view.getCamera().setViewCenter(440000, 2503000, 0);
-//        view.getCamera().setViewPercent(1);
+	
+	public static View view;
 
 
+	public static void main(String[] args) throws IOException, InterruptedException {      
+        
+		long startTime = System.nanoTime();
 
-
-
-        CustomGraphReader graphReader = new CustomGraphReader(args);
+		CustomGraphReader graphReader = new CustomGraphReader(args);
 		//run a read method on DAG
 		graphReader.readDAG();
-		SolutionTree solver = new SolutionTree(graphReader.getGraphAdapter().getAdjacencyList());
-		solver.calculateTime(solver.getRootNode(), solver.getNodeList());
-		
-		/*end timing
-			print statements to see the time and the minimum path time
-			long endTime = System.nanoTime();
-			long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
-			System.out.println(duration+" miliseconds");
-			System.out.println("Minimum time to do all tasks "+solver.getMinimumTime());
-			for(NodeObject a: solver.getBestSchedule()){
-				System.out.println("Node_name= "+a.getNodeName() + " Processor= " + (a.getProcessor()+1) + " Start_time= " + a.getStartTime() + " End_time= " + a.getEndTime());
-			}
-		*/
-		//Create .dot file at the end
-		DotWriter writer = new DotWriter();
-		writer.createDot(solver.getBestSchedule(),UserOptions.getInstance(),graphReader.getEdgeList(),solver.getInputGraph());
+		if(UserOptions.getInstance().isVisible() && UserOptions.getInstance().isParallel()){
+			
+			
+			
+			
+			
+			
+			
+		} else if(UserOptions.getInstance().isVisible() && !UserOptions.getInstance().isParallel()){
+			isVisual();
+			SolutionTreeVisual solver = new SolutionTreeVisual(graphReader.getGraphAdapter().getAdjacencyList());
+			solver.calculateTime(solver.getRootNode(), solver.getNodeList());
 
-        SolutionTree.bestTimeTree.addAttribute("ui.stylesheet", "graph{fill-color: rgb(60,141,13);}");
+			
+			//end timing
+			//	print statements to see the time and the minimum path time
+				long endTime = System.nanoTime();
+				long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+				System.out.println(duration+" miliseconds");
+				System.out.println("Minimum time to do all tasks "+solver.getMinimumTime());
+				for(NodeObject a: solver.getBestSchedule()){
+					System.out.println("Node_name= "+a.getNodeName() + " Processor= " + (a.getProcessor()+1) + " Start_time= " + a.getStartTime() + " End_time= " + a.getEndTime());
+				}
+			//Create .dot file at the end
+			DotWriter writer = new DotWriter();
+			writer.createDot(solver.getBestSchedule(),UserOptions.getInstance(),graphReader.getEdgeList(),solver.getInputGraph());
+			System.out.println(solver.nodeNumber);
+			System.out.println("HI");
+
+			SolutionTreeVisual.bestTimeTree.addAttribute("ui.stylesheet", "graph{fill-color: green;}");
+
+		} else if(UserOptions.getInstance().isParallel() && !UserOptions.getInstance().isVisible()){
+			
+		} else {
+			SolutionTree solver = new SolutionTree(graphReader.getGraphAdapter().getAdjacencyList());
+			solver.calculateTime(solver.getRootNode(), solver.getNodeList());
+			
+			
+			//end timing
+			//	print statements to see the time and the minimum path time
+				long endTime = System.nanoTime();
+				long duration = (endTime - startTime)/1000000;  //divide by 1000000 to get milliseconds.
+				System.out.println(duration+" miliseconds");
+				System.out.println("Minimum time to do all tasks "+solver.getMinimumTime());
+				for(NodeObject a: solver.getBestSchedule()){
+					System.out.println("Node_name= "+a.getNodeName() + " Processor= " + (a.getProcessor()+1) + " Start_time= " + a.getStartTime() + " End_time= " + a.getEndTime());
+				}
+			//Create .dot file at the end
+			DotWriter writer = new DotWriter();
+			writer.createDot(solver.getBestSchedule(),UserOptions.getInstance(),graphReader.getEdgeList(),solver.getInputGraph());
+			System.out.println(solver.nodeNumber);
+			System.out.println("HI");
+		}
+	}
+
+	private static void isVisual() {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		SolutionTreeVisual.bestTimeTree.addAttribute("ui.stylesheet", "node { " +
+				"size:12px; " +
+				"text-size:12px; " +
+				"text-alignment:above;" +
+				"text-padding: 3px;" +
+				"text-background-mode:rounded-box;" +
+				"}" +
+				"edge { " +
+				"text-size:13px; " +
+				"text-background-mode:rounded-box;" +
+				"size:5px;" +
+				"}" +
+				"graph{" +
+				"fill-color:#D6D8DD;" +
+				"}");
+		SolutionTreeVisual.bestTimeTree.addAttribute("ui.antialias");
+		SolutionTreeVisual.bestTimeTree.setStrict(false);
+		SolutionTreeVisual.bestTimeTree.addAttribute("ui.quality");
+		Viewer treeViewer = new Viewer(SolutionTreeVisual.bestTimeTree, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+		JFrame frame = new JFrame("Visualization");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel(new BorderLayout());
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		Dimension dim = new Dimension((int)(screenSize.getWidth()*0.9), (int)(screenSize.height/1.2));
+		ViewPanel tV = treeViewer.addDefaultView(false);
+
+		JPanel statPanel = new JPanel();
+		statPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
+		statPanel.setLayout(new BoxLayout(statPanel, BoxLayout.Y_AXIS));
+
+		JPanel bestPathStats = new JPanel();
+		bestPathStats.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		bestPathStats.add(SolutionTreeVisual.bestTimeLabel);
+		bestPathStats.add(SolutionTreeVisual.bestTimeScheduleLabel);
+		bestPathStats.add(SolutionTreeVisual.processorEndTimeLabel);
+		bestPathStats.add(SolutionTreeVisual.bestTimeCountLabel);
+		statPanel.add(bestPathStats);
+
+		JPanel countPanel = new JPanel();
+		countPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		countPanel.add(SolutionTreeVisual.nodesSearchedLabel);
+		countPanel.add(SolutionTreeVisual.equalBestTimeCountLabel);
+		statPanel.add(countPanel);
+
+		JPanel validSchedulePanel = new JPanel();
+		validSchedulePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		validSchedulePanel.add(SolutionTreeVisual.validScheduleCountLabel);
+		validSchedulePanel.add(SolutionTreeVisual.validScheduleLabel);
+		statPanel.add(validSchedulePanel);
+
+		JPanel processorPanel = new JPanel();
+		processorPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		processorPanel.add(SolutionTreeVisual.processorsUsedLabel);
+		processorPanel.add(SolutionTreeVisual.idleProcessorsLabel);
+		statPanel.add(processorPanel);
+
+
+		tV.setPreferredSize(dim);
+		panel.add(tV, BorderLayout.CENTER);
+		panel.add(statPanel, BorderLayout.PAGE_START);
+
+		frame.getContentPane().add(panel);
+		frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+
+		frame.setVisible(true);
 	}
 
 }
---
+++ 306Project/306 Project/src/visual/SolutionTreeVisual.java
@@ -0,0 +1,473 @@
+package visual;
+
+import data_structures.AdjacencyList;
+import data_structures.NodeObject;
+import data_structures.UserOptions;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * This class creates a solution tree from the input adjacency list.
+ * 
+ *
+ */
+public class SolutionTreeVisual {
+	public static Graph bestTimeTree = new SingleGraph("Best Time Tree");
+//	public static Graph datGraph = new SingleGraph("Directed Acyclic Task Graph");
+    private int nid = 0;
+
+	public static JLabel bestTimeLabel = new JLabel("Current Best Time:");
+	public static JLabel bestTimeScheduleLabel = new JLabel("Best Schedule:");
+	public static JLabel bestTimeCountLabel = new JLabel("Optimal Schedules Found:");
+	public static JLabel equalBestTimeCountLabel = new JLabel("Duplicate Best Schedules Found:");
+
+	public static JLabel nodesSearchedLabel = new JLabel("Nodes Searched:");
+	public static JLabel processorsUsedLabel = new JLabel("Processors Used:");
+	public static JLabel idleProcessorsLabel = new JLabel("Idle Processors:");
+	public static JLabel processorEndTimeLabel = new JLabel("Processor End Time:");
+
+	public static JLabel validScheduleCountLabel = new JLabel("Valid Schedules Discovered:");
+	public static JLabel validScheduleLabel = new JLabel("Current Valid Schedule:");
+
+	public long nodeNumber = 0;
+	private long bestTimeCount = 0;
+	private long validScheduleCount = 0;
+	private long equalBestTimeCount = 0;
+	// Stores the time for the current shortest schedule.
+	private static int minimumTime = Integer.MAX_VALUE;
+	// A list containing the current best schedule.
+	private static List<NodeObject> bestSchedule = new ArrayList<NodeObject>();
+	
+	private AdjacencyList inputGraph;
+	private NodeObject rootNode;
+	private List<String> nodeList;
+	private int numberofProcessors;
+	/**
+	 * Constructor that initialises the adjacency list to this class and makes a list of all nodes to use when checking if a node has been seen or not
+	 * @param inputGraph
+	 */
+	public SolutionTreeVisual(AdjacencyList inputGraph){
+		this.inputGraph = inputGraph;
+		nodeList = new ArrayList<String>();
+		for(String entry : inputGraph.getNodes()){
+    		nodeList.add(entry);
+    	}
+		numberofProcessors = UserOptions.getInstance().getProcessors();
+		//create the root node object
+		rootNode = new NodeObject(0, new ArrayList<NodeObject>(), "rootNode", new int[numberofProcessors], 0, 0);
+	}
+	
+	/** 
+	 * Pseudocode for recursively calculating the time taken to execute each path
+	 * in the solution tree.
+	 * @param nodesToCheck - List of nodes that have not yet been seen in this branch
+	 * of the solution tree
+	 * @param currentNode - the current node 
+	 * @throws InterruptedException 
+	 */
+	public void calculateTime(NodeObject currentNode, List<String> nodesToCheck) throws InterruptedException{
+
+		// Exit condition for exiting recursion
+		if(nodesToCheck.size() == 0){
+			validScheduleCount++;
+			validScheduleCountLabel.setText("Valid Schedules Discovered: "+Long.toString(validScheduleCount));
+
+			String procText = new String();
+			ArrayList procList = new ArrayList();
+			for(NodeObject node : currentNode.getCurrentPath()){
+				int processor = node.getProcessor();
+				if(!procList.contains(processor)){
+					procList.add(processor);
+					procText += "["+Integer.toString(processor+1)+"] ";
+				}
+			}
+			String idleText = new String();
+			for(int i = 0; i < numberofProcessors; i++){
+				if(!procList.contains(i)){
+					idleText += "["+Integer.toString(i+1)+"] ";
+				}
+			}
+			if (idleText.isEmpty()){
+				idleText = "None";
+			}
+			processorsUsedLabel.setText("Processors Used: "+ procText);
+			idleProcessorsLabel.setText("Idle Processors: "+ idleText);
+
+			String currentValidSchedule = new String();
+			for(NodeObject node : currentNode.getCurrentPath()){
+				if(!(node.getNodeName()=="rootNode")){
+					currentValidSchedule += node.getNodeName() +"("+(node.getProcessor()+1)+")";
+				}
+			}
+
+			validScheduleLabel.setText("Current Valid Schedule: "+currentValidSchedule);
+
+			// Calculate time
+			// Compare with minimumTime to see if this solution is better
+			if(maxTimeAtPoint(currentNode) < minimumTime) {
+
+				bestTimeCount++;
+				//when tree all the way down, and the time is lower than the global flag, set the new time
+				//and set the new schedule to it
+				for (Edge e : bestTimeTree.getEdgeSet()) {
+					e.addAttribute("ui.style", "fill-color:#ADB5C7;");
+				}
+				minimumTime = maxTimeAtPoint(currentNode);
+				bestTimeLabel.setText("Current Best Time: " + maxTimeAtPoint(currentNode));
+				bestTimeCountLabel.setText("Faster Schedules Found: " + Long.toString(bestTimeCount));
+				bestSchedule = currentNode.getCurrentPath();
+                int[] endArray = endArray(bestSchedule);
+                String endArrayString = new String();
+                int procID = 1;
+                for(int eA : endArray){
+                    endArrayString+=Integer.toString(procID)+": "+eA+" ";
+                    procID++;
+                }
+                processorEndTimeLabel.setText("Processor End Time: "+endArrayString);
+				String oldNode = new String();
+				String newNode = new String();
+				int i = 0;
+				Edge e;
+				String bestPath = new String();
+				for (NodeObject node : currentNode.getCurrentPath()) {
+					if (node.getNodeName().equals("rootNode"))
+						continue;
+					newNode += node.getNodeName() + node.getProcessor();
+					if (bestTimeTree.getNode(newNode) == null) {
+						Node n = bestTimeTree.addNode(newNode);
+
+						n.addAttribute("ui.label", node.getNodeName() + "(" + (node.getProcessor()+1) + ")");
+//                        n.addAttribute("ui.label", nid);
+						n.addAttribute("layout.frozen");
+
+						n.addAttribute("y", -i *15);
+						if (i == 0)
+							n.addAttribute("x", nid);
+						if (i > 0) {
+							if ((int) bestTimeTree.getNode(oldNode).getAttribute("x") < 0) {
+								n.addAttribute("x", -java.lang.Math.abs(nid));
+							} else {
+								n.addAttribute("x", java.lang.Math.abs(nid));
+							}
+						}
+					}
+					if (i > 0) {
+						bestTimeTree.removeEdge(newNode, oldNode);
+						e = bestTimeTree.addEdge(Integer.toString(nid), newNode, oldNode);
+						e.setAttribute("ui.style", "fill-color:red;");
+
+					}
+
+					oldNode = newNode;
+					i++;
+					if (nid < 0)
+						nid--;
+					else
+						nid++;
+					//SPEED FACTOR: lower=faster
+					Thread.sleep(50);
+					bestPath += node.getNodeName() + "(" + (node.getProcessor() + 1) + ") ";
+				}
+				bestTimeScheduleLabel.setText("Best Path: " + bestPath);
+				nid *= -1;
+			} else if(maxTimeAtPoint(currentNode) == minimumTime){
+				equalBestTimeCount++;
+				equalBestTimeCountLabel.setText("Duplicate Schedules at Current Optimal Time Found: "+Long.toString(equalBestTimeCount));
+			}
+			return;
+			}
+		
+		if(minimumTime != Integer.MAX_VALUE){
+			//if the time of current node but has not finished path is greater than optimal path which has finished dont bother looking
+			if(maxTimeAtPoint(currentNode) >= minimumTime){
+				return;
+			}
+		
+			//if(calculateLowerBound(currentNode, nodesToCheck) >= minimumTime){
+				//return;
+			//}
+		}
+
+		// Look through the list of unseen nodes and recursively call this method on nodes
+		// that do not have any parents on the nodesToCheck list.
+		for(String nodeToCheckStr : nodesToCheck){
+			if(isValidOption(nodeToCheckStr, nodesToCheck)){
+				int count = 0;
+				for(int i = 0 ; i <numberofProcessors; i++){
+					if(currentNode.getTimeWeightOnEachProcessor()[i] == 0){
+						count++;
+					}
+				}
+				int killtree = 0;
+				if(count >= 2){
+					killtree = count -1;
+				}
+				for(int j = 0; j < (numberofProcessors - killtree); j++){
+					
+					//UPDATE THE NEW NODE FOR RECURSION
+					
+					//create a newpath that is the same as current which includes the currentNode as well
+					//same thing but only copying the processor array --not checking for times at this place
+					ArrayList<NodeObject> nextPath = new ArrayList<NodeObject>(currentNode.getCurrentPath());
+					int[] processorArray = Arrays.copyOf(currentNode.getTimeWeightOnEachProcessor(), currentNode.getTimeWeightOnEachProcessor().length);
+					
+					//initialising the fields for the new NodalObject to recurse through
+					String newNodeName = nodeToCheckStr;
+					int newProcessor = j;
+					int nodalWeight = getNodalWeight(newNodeName);
+					int newStartTime = checkProcessStartTimeTask(currentNode, newNodeName, newProcessor);
+					int newEndTime = newStartTime+nodalWeight;
+					processorArray[newProcessor] = newEndTime;
+
+					//INITIALISE THE NEW NODE WITH UPDATED FIELDS
+					NodeObject nextNode = new NodeObject(newProcessor, nextPath, newNodeName, processorArray, newStartTime, newEndTime);
+					//copy the nodesToCheck list and need to remove the current node from it for recursion
+					List<String> newUpdatedListWithoutCurrentNode = new LinkedList<String>(nodesToCheck);
+					newUpdatedListWithoutCurrentNode.remove(newNodeName);
+					nodeNumber++;
+					nodesSearchedLabel.setText("Nodes Searched: "+Long.toString(nodeNumber));
+
+					//recursive call
+					calculateTime(nextNode, newUpdatedListWithoutCurrentNode);
+				}
+			}
+		}
+	}
+
+    private int[] endArray(List<NodeObject> nodeList){
+        int[] intArray = new int[numberofProcessors];
+        for(NodeObject n : nodeList){
+            if(n.getEndTime() > intArray[n.getProcessor()]){
+                intArray[n.getProcessor()] = n.getEndTime();
+            }
+        }
+        return intArray;
+    }
+
+
+    /**
+	 * Calculates the lower bound from any node. Lower bound is calculated by taking the 
+	 * current best time and adding all processes not yet executed and dividing it by the
+	 * number of processors.
+	 * @param currentNode: the whose lower bound has to be calculated
+	 * @param nodesToCheck: List of nodes not yet seen by the 
+	 * @return
+	 */
+	private int calculateLowerBound(NodeObject currentNode, List<String> nodesToCheck){
+		int currentMaxTime = minTimeAtPoint(currentNode); 
+		int currentworstTime = maxTimeAtPoint(currentNode);
+		int diff = currentworstTime - currentMaxTime;
+		int totalWeight = 0;
+		int currentWeight;
+		for(String nodeToCheckStr : nodesToCheck){
+			currentWeight = getNodalWeight(nodeToCheckStr);
+			totalWeight = totalWeight + currentWeight;
+		}
+		int lowerBound = currentMaxTime + ((totalWeight + diff) / numberofProcessors);
+		return lowerBound;	
+	}
+	
+	/**
+	 * Checks if the node is a valid option for option tree
+	 * @param node : The node we are checking the validity for.
+	 * @param nodesToCheck : List of unseen nodes at a given point in time
+	 * @return
+	 */
+	private boolean isValidOption(String node, List<String> nodesToCheck){
+		
+		//root node
+		if(checkAdjacencyListNullMap(node)){ 
+			return true;
+		}
+		
+			// valid if element has no parent. 
+			//inside this if statement if a parent is present that has not been seen this will be set to true and return false
+			//for an invalid option
+			 if(checkValidSolutionDepency(node, nodesToCheck)){
+				return false;
+			}
+		return true;
+	}
+	
+	/**
+	 * Using the adjacency list, get the index of the String node using the indicies map.
+	 * Once the index is found, get the map in the adjanceny list and check the size of the map
+	 * If the map size is 0 then it is a source node that we have not seen so it is a valid option.
+	 * @param nodeName
+	 * @return
+	 */
+	public boolean checkAdjacencyListNullMap(String nodeName){
+
+		if(inputGraph.getAdjacencyList().get(nodeName).size() == 0){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Check if the node we are currently looking at has unseen parent nodes.
+	 * @param nodeName
+	 * @param nodesToCheck
+	 * @return
+	 */
+	public boolean checkValidSolutionDepency(String nodeName, List<String> nodesToCheck){
+		//get map for c
+		for(String entry : inputGraph.getAdjacencyList().get(nodeName).keySet()){
+			//we check to see if the nodesToCheck has a or b inside of it.
+			//if it does that means we have not seen a node that it is dependent on. So invalid
+			if(nodesToCheck.contains(entry)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Find the largest time in the processor assuming that the node is updated.
+	 * Each element in this array has the end time for the index processor (assuming index 0 = processor 1)
+	 * @param node
+	 * @return largest end time amongst all processors
+	 */
+	public int maxTimeAtPoint(NodeObject node){
+		int largest = -1;
+		for(int i: node.getTimeWeightOnEachProcessor()){
+			if(i > largest){
+				largest = i;
+			}
+		}
+		return largest;
+		
+	}
+	
+	/**
+	 * Find the largest time in the processor assuming that the node is updated.
+	 * Each element in this array has the end time for the index processor (assuming index 0 = processor 1)
+	 * TODO : fix comments
+	 * @param node
+	 * @return largest end time amongst all processors
+	 */
+	public int minTimeAtPoint(NodeObject node){
+		int smallest = Integer.MAX_VALUE;
+		for(int i: node.getTimeWeightOnEachProcessor()){
+			if(i < smallest){
+				smallest = i;
+			}
+		}
+		return smallest;
+		
+	}
+	
+	/**
+	 * checkProcessStartTimeTask finds the time when to add the task on the processor. This is required as need to check on the
+	 * constraints which are the communication costs. The dependencies constraint is met in the checkValidSolutionDepency method called
+	 * before. So just finds the start time for the particular task.
+	 * @return finds the start time for the particular task.
+	 */
+	public int checkProcessStartTimeTask(NodeObject currentNode, String newNode, int processor){
+		if(checkAdjacencyListNullMap(newNode)){
+			//returns the endtime for particular processor as the new start time if the node in question was a source node
+			//so it has no dependencies and can be placed directly at the end of any processor
+			return currentNode.getTimeWeightOnEachProcessor()[processor];
+		}else{
+			//has parents
+			int maxTime = -1;
+			int tmpTime = -1;
+			//iterate through the current path of nodes visited to see the latest time to add the particular node
+			for(NodeObject node: currentNode.getCurrentPath()){
+				//check parents if processor is same
+				if(inputGraph.isDependent(node,newNode)){
+					if(node.getProcessor() == processor){
+						//if it is same process, the next time it can go on is directly after it so endtime = starttime
+						tmpTime = node.getEndTime();
+					} else {
+						//if process are different; add communication cost so next time it can go on is directly after the communication cost.
+						tmpTime = node.getEndTime() + inputGraph.getEdgeWeight(node, newNode);
+					}
+				}
+				//getting the greater of the times, such that maxTime is the latest point where we can add that node in processor
+				//this is only based on the dependency rule. So only dependencies are checked to see if the constraints are met.
+				if(maxTime < tmpTime){
+					maxTime = tmpTime;
+				}
+			}
+			//This is the case when all dependencies are finished and the node can just be added at the end of the processor
+			//Can act as a source node since the startTime would be greater than the maxTime if added after a dependency.
+			if(currentNode.getTimeWeightOnEachProcessor()[processor] > maxTime){
+				return currentNode.getTimeWeightOnEachProcessor()[processor];
+			} else {
+				//if the dependency requires a time greater than the endtime of latest node in same processor. 
+				//Must mean there is a communication cost required from a different processor
+				//The startTime to run on current processor
+				return maxTime;
+			}
+		}
+	}
+	
+	/**
+	 * getting the nodal weight from a string of node name
+	 * @param node
+	 * @return
+	 */
+	public int getNodalWeight(String node){
+		if(node.equals("rootNode")){
+			return 0;
+		}
+		return inputGraph.getNodeWeights().get(node);
+	}
+
+	/*
+	 * GETTERS AND SETTERS FOR THIS CLASS
+	 */
+	
+	
+	public static int getMinimumTime() {
+		return minimumTime;
+	}
+
+	public static void setMinimumTime(int minimumTime) {
+		SolutionTreeVisual.minimumTime = minimumTime;
+	}
+
+	public static List<NodeObject> getBestSchedule() {
+		return bestSchedule;
+	}
+
+	public static void setBestSchedule(List<NodeObject> bestSchedule) {
+		SolutionTreeVisual.bestSchedule = bestSchedule;
+	}
+
+	public AdjacencyList getInputGraph() {
+		return inputGraph;
+	}
+
+	public void setInputGraph(AdjacencyList inputGraph) {
+		this.inputGraph = inputGraph;
+	}
+
+	public NodeObject getRootNode() {
+		return rootNode;
+	}
+
+	public void setRootNode(NodeObject rootNode) {
+		this.rootNode = rootNode;
+	}
+
+	public List<String> getNodeList() {
+		return nodeList;
+	}
+
+	public void setNodeList(List<String> nodeList) {
+		this.nodeList = nodeList;
+	}
+
+	
+}
