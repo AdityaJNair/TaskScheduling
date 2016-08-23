ab08f14ae92b24aa62a4f30caf5385c74504f0c3
JunoJin <jjin525@aucklanduni.ac.nz>
2016-08-23 12:56:51 +1200

    Visualization - deleting DAG, expanding statistics

--- 306Project/306 Project/src/main/Main.java
+++ 306Project/306 Project/src/main/Main.java
@@ -1,19 +1,16 @@
 package main;
-import java.awt.geom.Arc2D;
-import java.io.IOException;
-
-import org.graphstream.ui.swingViewer.ViewPanel;
-import org.graphstream.ui.view.View;
-import org.graphstream.ui.view.Viewer;
-
-import javax.swing.*;
-import java.awt.*;
-
 import data_structures.NodeObject;
 import data_structures.UserOptions;
 import graph306.CustomGraphReader;
 import graph306.SolutionTree;
+import org.graphstream.ui.swingViewer.ViewPanel;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.Viewer;
 import visual.SolutionTreeVisual;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class Main {
 	
@@ -30,91 +27,13 @@ public class Main {
 		if(UserOptions.getInstance().isVisible() && UserOptions.getInstance().isParallel()){
 			
 			
-			//if parallel and visual
+			
 			
 			
 			
 			
 		} else if(UserOptions.getInstance().isVisible() && !UserOptions.getInstance().isParallel()){
-<<<<<<< HEAD
-			
-			
-			//if visual only
-=======
-
->>>>>>> b91121853718c3550f4f0672790562207a699a0f
-			System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-			SolutionTreeVisual.bestTimeTree.addAttribute("ui.stylesheet", "node { " +
-					"size:6px; " +
-					"text-color:red; " +
-					"text-size:9px; " +
-					"text-alignment:above;" +
-					"text-background-mode:rounded-box;" +
-					"}" +
-					"edge { " +
-					"text-color:red; " +
-					"text-size:13px; " +
-					"text-background-mode:rounded-box;" +
-					"size:3px;" +
-					"}" +
-					"graph {" +
-					"fill-color:yellow;" +
-					"}");
-			SolutionTreeVisual.datGraph.addAttribute("ui.stylesheet", "node { " +
-					"size:60px; " +
-					"text-color:black; " +
-					"text-size:15px; " +
-					"stroke-mode:plain;" +
-					"fill-color:magenta;" +
-					"}" +
-					"edge { " +
-					"text-color:red; " +
-					"text-size:13px; " +
-					"text-background-mode:rounded-box;" +
-					"size:1px;" +
-					"}" +
-					"graph {" +
-					"fill-color:yellow;" +
-					"}");
-			SolutionTreeVisual.bestTimeTree.addAttribute("ui.antialias");
-			SolutionTreeVisual.bestTimeTree.setStrict(false);
-			SolutionTreeVisual.bestTimeTree.addAttribute("ui.quality");
-			SolutionTreeVisual.datGraph.addAttribute("ui.antialias");
-			SolutionTreeVisual.datGraph.setStrict(false);
-			SolutionTreeVisual.datGraph.addAttribute("ui.quality");
-			Viewer treeViewer = new Viewer(SolutionTreeVisual.bestTimeTree, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-			Viewer graphViewer = new Viewer(SolutionTreeVisual.datGraph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-			graphViewer.enableAutoLayout();
-			JFrame frame = new JFrame("Visualization");
-
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-			JPanel panel = new JPanel(new BorderLayout());
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-            Dimension dim = new Dimension((int)(screenSize.getWidth()/1.5), screenSize.height/3);
-			ViewPanel tV = treeViewer.addDefaultView(false);
-
-            JPanel statPanel = new JPanel(new FlowLayout());
-
-            statPanel.add(SolutionTreeVisual.bestTimeLabel);
-            statPanel.add(SolutionTreeVisual.nodesSearchedLabel);
-            statPanel.add(SolutionTreeVisual.bestTimeCountLabel);
-            statPanel.add(SolutionTreeVisual.validScheduleCountLabel);
-            statPanel.add(SolutionTreeVisual.bestTimeScheduleLabel);
-
-			tV.setPreferredSize(dim);
-			panel.add(tV, BorderLayout.CENTER);
-            panel.add(statPanel, BorderLayout.PAGE_START);
-			ViewPanel gV = graphViewer.addDefaultView(false);
-			gV.setPreferredSize(dim);
-			panel.add(gV, BorderLayout.PAGE_END);
-//        panel.add(gV, BorderLayout.PAGE_END);
-            frame.getContentPane().add(panel);
-			frame.setVisible(true);
-			frame.pack();
-
-	        
+			isVisual();
 			SolutionTreeVisual solver = new SolutionTreeVisual(graphReader.getGraphAdapter().getAdjacencyList());
 			solver.calculateTime(solver.getRootNode(), solver.getNodeList());
 			
@@ -132,15 +51,12 @@ public class Main {
 			writer.createDot(solver.getBestSchedule(),UserOptions.getInstance(),graphReader.getEdgeList(),solver.getInputGraph());
 			System.out.println(solver.nodeNumber);
 			System.out.println("HI");
-			
-			
-			
+
+			SolutionTreeVisual.bestTimeTree.addAttribute("ui.stylesheet", "graph{fill-color: green;}");
+
 		} else if(UserOptions.getInstance().isParallel() && !UserOptions.getInstance().isVisible()){
 			
-			//parallel only
 		} else {
-			
-			//normal
 			SolutionTree solver = new SolutionTree(graphReader.getGraphAdapter().getAdjacencyList());
 			solver.calculateTime(solver.getRootNode(), solver.getNodeList());
 			
@@ -160,6 +76,80 @@ public class Main {
 			System.out.println(solver.nodeNumber);
 			System.out.println("HI");
 		}
+	}
+
+	private static void isVisual() {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		SolutionTreeVisual.bestTimeTree.addAttribute("ui.stylesheet", "node { " +
+				"size:12px; " +
+				"text-color:#AD2A1A; " +
+				"text-size:12px; " +
+				"text-alignment:above;" +
+				"text-padding: 3px;" +
+				"text-background-mode:rounded-box;" +
+				"}" +
+				"edge { " +
+				"text-color:#AD2A1A; " +
+				"text-size:13px; " +
+				"text-background-mode:rounded-box;" +
+				"size:6px;" +
+				"}" +
+				"graph{" +
+				"fill-color:#FFFFAA;" +
+				"}");
+		SolutionTreeVisual.bestTimeTree.addAttribute("ui.antialias");
+		SolutionTreeVisual.bestTimeTree.setStrict(false);
+		SolutionTreeVisual.bestTimeTree.addAttribute("ui.quality");
+		Viewer treeViewer = new Viewer(SolutionTreeVisual.bestTimeTree, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
+		JFrame frame = new JFrame("Visualization");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel(new BorderLayout());
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		Dimension dim = new Dimension((int)(screenSize.getWidth()*0.9), (int)(screenSize.height/1.2));
+		ViewPanel tV = treeViewer.addDefaultView(false);
+
+		JPanel statPanel = new JPanel();
+		statPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
+		statPanel.setLayout(new BoxLayout(statPanel, BoxLayout.Y_AXIS));
+
+		JPanel bestPathStats = new JPanel();
+		bestPathStats.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		bestPathStats.add(SolutionTreeVisual.bestTimeLabel);
+		bestPathStats.add(SolutionTreeVisual.bestTimeScheduleLabel);
+		bestPathStats.add(SolutionTreeVisual.bestTimeCountLabel);
+		bestPathStats.add(SolutionTreeVisual.equalBestTimeCountLabel);
+		statPanel.add(bestPathStats);
+
+		JPanel processorPanel = new JPanel();
+		processorPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		processorPanel.add(SolutionTreeVisual.nodesSearchedLabel);
+		processorPanel.add(SolutionTreeVisual.processorsUsedLabel);
+		processorPanel.add(SolutionTreeVisual.idleProcessorsLabel);
+		processorPanel.add(SolutionTreeVisual.partialScheduleLabel);
+		processorPanel.add(SolutionTreeVisual.processorEndTimeLabel);
+		statPanel.add(processorPanel);
+
+		JPanel validSchedulePanel = new JPanel();
+		validSchedulePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		validSchedulePanel.add(SolutionTreeVisual.validScheduleCountLabel);
+		validSchedulePanel.add(SolutionTreeVisual.validScheduleLabel);
+		statPanel.add(validSchedulePanel);
+
+		JPanel boundValuePanel = new JPanel();
+		boundValuePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 2));
+		boundValuePanel.add(SolutionTreeVisual.boundValueLabel);
+		statPanel.add(boundValuePanel);
+
+
+		tV.setPreferredSize(dim);
+		panel.add(tV, BorderLayout.CENTER);
+		panel.add(statPanel, BorderLayout.PAGE_START);
+
+		frame.getContentPane().add(panel);
+		frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+
+		frame.setVisible(true);
 	}
 
 }
